b0881a3b6c01efae483d4a50cb15d57b3a958f59
Ketan_Patil <Ketan_Patil@symantec.com>
2017-09-22 18:28:39 +0530

    more exploring

--- Lynda1-Exploring lake pend oreille/src/Main.java
+++ Lynda1-Exploring lake pend oreille/src/Main.java
@@ -1,9 +1,33 @@
+import java.io.File;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		
+		Scanner scanner  = new Scanner(System.in);
+		
+		String inputs[] = {"","",""};
+		
+		System.out.println("Enter a year (2014 - 2015)");
+		inputs[0] = scanner.nextLine();
+		
+		System.out.println("Enter a month (1 - 12)");
+		inputs[1] = scanner.nextLine();
+		
+		System.out.println("Enter a day (1 - 31)");
+		inputs[2] = scanner.nextLine();
+		
+		Reader reader = new Reader("Sensor_Data/"+inputs[0]+"/"+inputs[0]+".txt");
+		
+		
+		
+		
+		scanner.close();
 	}
+	
 
 }
+
+
 	
---
+++ Lynda1-Exploring lake pend oreille/src/Reader.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+public class Reader {
+	
+	private File inputFile;
+
+	public Reader(String fileName) {
+	System.out.println(fileName);	
+//	 inputFile = new File(fileName);
+	
+	} 
+	
+	
+}
