1d5008daacd94d141f508f18f05ba3ecf9dc45cf
Ketan_Patil <Ketan_Patil@symantec.com>
2017-09-22 21:09:29 +0530

    initializing(2.0)

--- Lynda1-Exploring lake pend oreille/src/Main.java
+++ Lynda1-Exploring lake pend oreille/src/Main.java
@@ -1,33 +1,32 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		Scanner scanner  = new Scanner(System.in);
 		
-		String inputs[] = {"","",""};
+		String inputDate;
 		
-		System.out.println("Enter a year (2014 - 2015)");
-		inputs[0] = scanner.nextLine();
+		System.out.println("Enter a date in \"YYYY_MM_DD\" format (Ex : 2014_07_30) for years 2013-2015");
+		inputDate = scanner.nextLine();
 		
-		System.out.println("Enter a month (1 - 12)");
-		inputs[1] = scanner.nextLine();
+		String year = inputDate.substring(0, 4);
 		
-		System.out.println("Enter a day (1 - 31)");
-		inputs[2] = scanner.nextLine();
+		String filePath = "Sensor_Data/"+year+"/"+year+".txt";
 		
-		Reader reader = new Reader("Sensor_Data/"+inputs[0]+"/"+inputs[0]+".txt");
+		Reader reader = new Reader(filePath);
 		
-		ArrayList<Double> airTemprature = reader.getData(Macros.AIR_TEMP, inputs);
-		ArrayList<Double> barometricPressure = reader.getData(Macros.BAROMETRIC_PRESSURE, inputs);
-		ArrayList<Double> dewPoint = reader.getData(Macros.DEW_POINT, inputs);
-		ArrayList<Double> relativeHumidity = reader.getData(Macros.RELATIVE_HUMIDITY, inputs);
-		ArrayList<Double> windDirection = reader.getData(Macros.WIND_DIRECTION, inputs);
-		ArrayList<Double> windGust = reader.getData(Macros.WIND_GUST, inputs);
-		ArrayList<Double> windSpeed = reader.getData(Macros.WIND_SPEED, inputs);
+		ArrayList<Double> airTemprature = reader.getData(Macros.AIR_TEMP, inputDate);
+		ArrayList<Double> barometricPressure = reader.getData(Macros.BAROMETRIC_PRESSURE, inputDate);
+		ArrayList<Double> dewPoint = reader.getData(Macros.DEW_POINT, inputDate);
+		ArrayList<Double> relativeHumidity = reader.getData(Macros.RELATIVE_HUMIDITY, inputDate);
+		ArrayList<Double> windDirection = reader.getData(Macros.WIND_DIRECTION, inputDate);
+		ArrayList<Double> windGust = reader.getData(Macros.WIND_GUST, inputDate);
+		ArrayList<Double> windSpeed = reader.getData(Macros.WIND_SPEED, inputDate);
 		
 		
 		
--- Lynda1-Exploring lake pend oreille/src/Reader.java
+++ Lynda1-Exploring lake pend oreille/src/Reader.java
@@ -1,4 +1,8 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Reader {
@@ -8,12 +12,25 @@ public class Reader {
 	public Reader(String fileName) {
 		
 		inputFile = new File(fileName);
-	
+		
 	} 
 	
-	public ArrayList<Double> getData(double offset,String[] date){
+	public ArrayList<Double> getData(double offset,String inputDate) throws IOException{
+		
+		
+		
+		BufferedReader fileReader = new BufferedReader(new FileReader(inputFile));
+		String currentLine;
+		
+		while ((currentLine = fileReader.readLine()) != null){
+			if(currentLine.startsWith(inputDate)){
+				System.out.println(currentLine);
+			}
+		}
+		
+		
 		return null;
 	}
 	
-	
+
 }
